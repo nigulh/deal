a0ce942eeea491e35f3e80d3a0e97a3f9a050d71
Hendrik Nigul <hendrik.nigul@gmail.com>
2015-06-13 16:58:35 +0300

    Refactor counters a bit

--- analyys/aleksis_lead_problem/lead_JT6_QT9_J98_KJT9.tcl
+++ analyys/aleksis_lead_problem/lead_JT6_QT9_J98_KJT9.tcl
@@ -5,48 +5,37 @@ source lib/parscore.tcl
 source format/none
 
 
-set cellWidth 1
+set cellWidth 5
 set numberPrecision 3
-set firstCellWidth 1
-
-proc normalizeCount { count totalCnt } {
-	global dealCnt
-	if {[string is integer -strict $count]} {
-		return [expr 100.0 * $count / $dealCnt]
-	}
-	return $count
-}
-
-proc debug { line } {
-	puts stderr $line
-}
-
-set rowLabels [ list . "SJ" "HQ" "HT" "DJ" "D9" "CK" "CJ" ]
-set colLabels [ list . "NO" 1 2 3 4 5 6 7 8 9 10 11 12 13 avg ]
+set firstCellWidth 5
 
 
-# Initialize table with all the labels
-proc initdb {} {
-	global vulnList permIndexes rowLabels colLabels
-	init s "Lead" $rowLabels $colLabels
-}
+set leads [ list "JS" "6S" "QH" "TH" "JD" "9D" "KC" "JC" ]
+set rowLabels [ concat [list . ] $leads]
+set colLabels [ list . 8 9 10 11 "x" sum sdev avg]
+init s "Lead" $rowLabels $colLabels
+init dealCount "Accepted deals"
+init allDeals "All deals"
 
 proc analyze { } {
-	global tricks suitPermutations vulnList permIndexes
-        
-        increment s "SJ" [dds -leader west -trick JS south hearts]
-        increment s "HQ" [dds -leader west -trick QH south hearts]
-        increment s "HT" [dds -leader west -trick TH south hearts]
-        increment s "DJ" [dds -leader west -trick JD south hearts]
-        increment s "D9" [dds -leader west -trick 9D south hearts]
-        increment s "CK" [dds -leader west -trick KC south hearts]
-        increment s "CJ" [dds -leader west -trick JC south hearts]
+        global leads
+       
+        foreach lead $leads { 
+            set t [dds -leader west -trick $lead south hearts]
+            while {$t == 0} {
+                puts stderr "[south] [north] [west] [east] $t"
+                set t [dds -leader west -trick $lead south hearts]
+            }
+            increment s $lead $t
+            if {$t < 10} { increment s $lead "x" }
+        } 
 }
 
 west is "JT6 QT9 J98 KJT9"
 
 main {
 	global dealCnt
+        increment allDeals
         
         if {[hcp south] < 22} {reject}
         if {[hcp south] > 24} {reject}
@@ -59,20 +48,16 @@ main {
         if {[spades south] < 2} {reject}
 	
 	incr dealCnt
-	if { $dealCnt == 1 } {
-		initdb
-		puts stderr "[south]"
-	}
+        puts -nonewline stderr "\r$dealCnt: [south]" 
+
+        increment dealCount
 	analyze
-	if { $dealCnt % 10 == 0 } { puts stderr "$dealCnt" }
 	accept
 }
 
 deal_finished {
-	global dealCnt
 	puts stderr ""
 	outputdb
-	puts stderr "Generated $dealCnt deals"
 }
 
 
--- lib/counters.tcl
+++ lib/counters.tcl
@@ -13,17 +13,16 @@
 
 proc isStatHeader { header } {
 	switch $header {
-		sum { return true }
-		avg { return true }
-		sdev { return true }
+		sum -
+		avg -
+		sdev { return 1 }
+                default { return 0 }
 	}
-	return false
 }
 
 proc init0d { dbVar var } {
 	upvar 1 $dbVar db
 	dict set db $var data 0
-	dict set db $var totalCnt 1
 }
 
 proc init1d { dbVar var rows } {
@@ -32,11 +31,11 @@ proc init1d { dbVar var rows } {
 	foreach row $rows {
 		if { [isStatHeader $row] } {
 			sdev $var,avg,
+                        dict set db $var withRowStats 1
 		} else {
 			dict set db $var data $row, 0
 		}
 	}
-	dict set db $var totalCnt 0
 }
 
 proc init2d { dbVar var rows cols } {
@@ -47,14 +46,15 @@ proc init2d { dbVar var rows cols } {
 		if { [isStatHeader $row] } {
 			sdev $var,avg,$col
 			sdev $var,avg,sum
+                        dict set db $var withRowStats 1
 		} elseif { [isStatHeader $col] } {
 			sdev $var,$row,avg
 			sdev $var,sum,avg
+                        dict set db $var withColumnStats 1
 		} else {
 			dict set db $var data $row,$col 0
 		}
 	} }
-        dict set db $var totalCnt 0
 }
 
 proc init { var desc args } {
@@ -62,6 +62,9 @@ proc init { var desc args } {
 	dict set tab $var desc $desc
 	dict set tab $var var $var
 	dict set tab $var dim [llength $args]
+        dict set tab $var withRowStats 0
+        dict set tab $var withColumnStats 0
+	dict set tab $var totalCnt 0
 	switch [llength $args] {
 		0 { init0d tab $var }
 		1 { init1d tab $var [lindex $args 0] }
@@ -78,19 +81,22 @@ proc incr0d { dbVar var } {
 
 proc incr1d { dbVar var row } {
 	upvar 1 $dbVar db
-	
-	dict set db $var data $row, [ expr [dict get $db $var data $row,] + 1]
-	catch {$var,avg, add $row}
+	catch {dict set db $var data $row, [ expr [dict get $db $var data $row,] + 1]}
+	if {[dict get $db $var withRowStats]} {catch {$var,avg, add $row}}
         dict set db $var totalCnt [ expr [dict get $db $var totalCnt] + 1]
 }
 
 proc incr2d { dbVar var row col } {
 	upvar 1 $dbVar db
 	catch {dict set db $var data $row,$col [ expr [ dict get $db $var data $row,$col ] + 1]}
-	catch {$var,$row,avg add $col}
-	catch {$var,avg,$col add $row}
-	catch {$var,sum,avg add $col}
-	catch {$var,avg,sum add $row}
+        if {[dict get $db $var withColumnStats]} {
+            catch {$var,$row,avg add $col}
+            catch {$var,sum,avg add $col}
+        }
+        if {[dict get $db $var withRowStats]} {
+            catch {$var,avg,$col add $row}
+            catch {$var,avg,sum add $row}
+        }   
         dict set db $var totalCnt [ expr [dict get $db $var totalCnt] + 1]
 }
 
@@ -100,13 +106,11 @@ proc increment { var args } {
 	if { $dim != [llength $args]} {
 		return -code error "Variable $var is {$d}d, got $args"
 	}
-	if { [llength $args] == 0 } {
-		incr0d tab $var
-	} elseif { [llength $args] == 1 } {
-		incr1d tab $var [lindex $args 0]
-	} elseif { [llength $args] == 2 } {
-		incr2d tab $var [lindex $args 0] [lindex $args 1]
-	}
+        switch [llength $args] {
+	    0 { incr0d tab $var } 
+            1 { incr1d tab $var [lindex $args 0] } 
+            2 { incr2d tab $var [lindex $args 0] [lindex $args 1] }
+        }
 }
 
 
@@ -123,45 +127,52 @@ proc normalizeCount { count totalCnt } {
 	return $count
 }
 
-proc getOutputData { elem row col } {
-	if { $row == "." && $col == "."} { return "" }
-	if { $row == "."} { return $col }
-	if { $col == "."} { return $row }
-        set totalCnt [dict get $elem totalCnt]
-	if { ![isStatHeader $row] && ![isStatHeader $col] } { 
-		set ret [normalizeCount [ dict get $elem data $row,$col ] $totalCnt]
-	} else {
-		set var [dict get $elem var]
-		if { $row == "sum" && $col == "avg" } { set ret [ getStat $var,sum,avg average 1 ] 
-		} elseif { $row == "sum" && $col == "sdev" } { set ret [ getStat $var,sum,avg sdev 2 ] 
-		} elseif { $row == "avg" && $col == "sum" } { set ret [ getStat $var,avg,sum average 1 ] 
-		} elseif { $row == "sdev" && $col == "sum" } { set ret [ getStat $var,avg,sum sdev 2 ] 
-		} elseif { [isStatHeader $row] && [isStatHeader $col] } { return "" 
-		# Now there is only one statistical row/col
-		} elseif { $row == "sum" } { set ret [ normalizeCount [ $var,avg,$col count ] $totalCnt]
-		} elseif { $row == "avg" } { set ret [ getStat $var,avg,$col average 1 ] 
-		} elseif { $row == "sdev" } { set ret [ getStat $var,avg,$col sdev 2 ] 
-		} elseif { $col == "sum" } { set ret [ normalizeCount [ $var,$row,avg count ] $totalCnt] 
-		} elseif { $col == "avg" } { set ret [ getStat $var,$row,avg average 1] 
-		} elseif { $col == "sdev" } { set ret [ getStat $var,$row,avg sdev 2 ] 
-		} else { return "??" }
-	}
+proc getOutputData { elem row col statsFlags } {
+        set dataKey "[isStatHeader $row][isStatHeader $col],$row,$col"
+        switch -glob $dataKey {
+            *,.,. { return "" }
+            *,.,* { return $col }
+            *,*,. { return $row }
+        }
+	set var [dict get $elem var]
+        switch $statsFlags {
+            00 { set totalCnt 100 }
+            10 { set totalCnt [$var,avg,$col count] }
+            01 { set totalCnt [$var,$row,avg count] }
+            11 { set totalCnt [dict get $elem totalCnt] }
+        }
+        
+        switch -glob $dataKey {
+            00,* { set ret [normalizeCount [ dict get $elem data $row,$col ] $totalCnt] } 
+            11,sum,avg { set ret [ getStat $var,sum,avg average 1 ] }
+            11,sum,sdev { set ret [ getStat $var,sum,avg sdev 2 ] }
+            11,avg,sum { set ret [ getStat $var,avg,sum average 1 ] } 
+            11,sdev,sum { set ret [ getStat $var,avg,sum sdev 2 ] } 
+            11,* { return "" }
+            10,sum,* { set ret [ normalizeCount [ $var,avg,$col count ] $totalCnt] } 
+            10,avg,* { set ret [ getStat $var,avg,$col average 1 ] } 
+            10,sdev,* { set ret [ getStat $var,avg,$col sdev 2 ] } 
+            01,*,sum { set ret [ normalizeCount [ $var,$row,avg count ] $totalCnt] } 
+            01,*,avg { set ret [ getStat $var,$row,avg average 1] } 
+            01,*,sdev { set ret [ getStat $var,$row,avg sdev 2 ] } 
+            default { return "??" }
+        }
 	return [ formatNumber $ret ]
 }
 
 
-
-proc output0d {elem } {
-	puts "[dict get $elem desc]: [formatNumber [normalizeCount [dict get $elem data] 1]]"
+proc output0d {elem} {
+	puts "[dict get $elem desc]: [formatNumber [dict get $elem data]]"
 }
 
 proc output1d {elem} {
 	puts "[dict get $elem desc]:"
 	set data [dict get $elem data]
 	set showAsRow true
+        set statsFlags "[dict get $elem withRowStats][dict get $elem withColumnStats]"
 	if { !$showAsRow } {
 		foreach row [dict get $elem rows] {
-			set data [getOutputData $elem $row ""]
+			set data [getOutputData $elem $row "" $statsFlags]
 			puts "[formatCell $row] [formatCell $data]"
 		}
 	} else {
@@ -170,7 +181,7 @@ proc output1d {elem} {
 		}
 		puts ""
 		foreach row [dict get $elem rows] {
-			set data [getOutputData $elem $row ""]
+			set data [getOutputData $elem $row "" $statsFlags]
 			puts -nonewline "[formatCell $data]"
 		}
 		puts ""
@@ -180,6 +191,7 @@ proc output1d {elem} {
 proc output2d {elem} {
 	global firstCellWidth
 	set isFirst true
+        set statsFlags "[dict get $elem withRowStats][dict get $elem withColumnStats]"
 	foreach desc [split [dict get $elem desc] "\n"] {
 		if {!$isFirst} {puts ""}
 		puts -nonewline [formatCell $desc $firstCellWidth]
@@ -187,7 +199,7 @@ proc output2d {elem} {
 	}
 	foreach row [dict get $elem rows] {
 		foreach col [dict get $elem cols] {
-			set data [getOutputData $elem $row $col]
+			set data [getOutputData $elem $row $col $statsFlags]
 			if { $col == "." && $row == "." } { continue }
 			if { $col == "." } { set width $firstCellWidth } else { set width -1 }
 			puts -nonewline [formatCell $data $width]
@@ -197,16 +209,21 @@ proc output2d {elem} {
 	puts ""
 }
 
+proc output {key} {
+	global tab
+        set elem [ dict get $tab $key ]
+        set dim [ dict get $elem dim ]
+        switch $dim {
+            0 { output0d $elem }
+            1 { output1d $elem }
+            2 { output2d $elem }
+        }
+}
+
 proc outputdb { } {
 	global tab
 	foreach key [dict keys $tab] {
-		set val [ dict get $tab $key ]
-		set dim [ dict get $val dim ]
-		switch $dim {
-			0 { output0d $val }
-			1 { output1d $val }
-			2 { output2d $val }
-		}
+            output $key 
 	}
 }
 
